68f2f27eb6ce4c615c3324c69422476b4996fa1e
alexpac27 <alexpac27@gmail.com>
2020-06-25 22:19:38 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+
+Post.create(title: "title 1", description: "body 1")
+Post.create(title: "title 2", description: "body 2")
+Post.create(title: "title 3", description: "body 3")
